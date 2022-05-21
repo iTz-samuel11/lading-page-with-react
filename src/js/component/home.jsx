bd872450c5ba9a84476f9366c8e689c7c95d9356
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-21 23:48:44 +0000

    agrego barra de copyright

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
 		<React.Fragment>
 			<Nav />
 			<Cards />
+			<End />
 		</React.Fragment>
 	);
 };
@@ -20,12 +21,12 @@ const Nav = () => {
 		<nav className="navbar navbar-expand-lg navbar-light bg-dark ">
 			<div className="container-fluid">
 				<a
-					className="navbar-brand"
+					className="navbar-brand text-light"
 					href="#"
 					style={{
 						color: "white",
-						marginRight: "72%",
-						marginLeft: "3%",
+						marginRight: "68%",
+						marginLeft: "5%",
 					}}>
 					{"Start Bootstrap"}
 				</a>
@@ -39,7 +40,9 @@ const Nav = () => {
 					aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
-				<div className="collapse navbar-collapse" id="navbarNav">
+				<div
+					className="collapse navbar-collapse float-right"
+					id="navbarNav">
 					<ul className="navbar-nav">
 						<li className="nav-item">
 							<a
@@ -77,88 +80,104 @@ const Cards = () => {
 			<div className="justifi-content-center m-0 row my-3">
 				<div className="card">
 					<div className="card-body">
-						<h1 className="card-title">{"A Warm Welcome!"}</h1>
-						<p className="card-text">
+						<h1 className="card-title fs-1">{"A Warm Welcome!"}</h1>
+						<p className="card-text fs-5">
 							{
 								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Ipsa, ipsam,eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."
 							}
 						</p>
-						<a href="#" className="btn btn-primary">
+						<a href="#" className="btn btn-primary fs-5">
 							{"Call to action!"}
 						</a>
 					</div>
 				</div>
 			</div>
 			<div className="d-sm-flex">
-				<div className="card ">
+				<div className="card text-center">
 					<img
 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
 						className="card-img-top"
 						alt="..."
 					/>
 					<div className="card-body">
-						<h4 className="card-title">Card title</h4>
+						<h4 className="card-title">{"Card title"}</h4>
 						<p className="card-text">
-							Some quick example text to build on the card title
-							and make up the bulk of the card's content.
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
 						</p>
 						<a href="#" className="btn btn-primary">
-							Go somewhere
+							{"Find Out More!"}
 						</a>
 					</div>
 				</div>
-				<div className="card mx-1">
+				<div className="card mx-1 text-center">
 					<img
 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
 						className="card-img-top"
 						alt="..."
 					/>
 					<div className="card-body">
-						<h4 className="card-title">Card title</h4>
+						<h4 className="card-title">{"Card title"}</h4>
 						<p className="card-text">
-							Some quick example text to build on the card title
-							and make up the bulk of the card's content.
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
 						</p>
 						<a href="#" className="btn btn-primary">
-							Go somewhere
+							{"Find Out More!"}
 						</a>
 					</div>
 				</div>
-				<div className="card me-1">
+				<div className="card me-1 text-center">
 					<img
 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
 						className="card-img-top"
 						alt="..."
 					/>
 					<div className="card-body">
-						<h4 className="card-title">Card title</h4>
+						<h4 className="card-title">{"Card title"}</h4>
 						<p className="card-text">
-							Some quick example text to build on the card title
-							and make up the bulk of the card's content.
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
 						</p>
 						<a href="#" className="btn btn-primary">
-							Go somewhere
+							{"Find Out More!"}
 						</a>
 					</div>
 				</div>
-				<div className="card">
+				<div className="card text-center">
 					<img
 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
 						className="card-img-top"
 						alt="..."
 					/>
-					<div className="card-body">
-						<h4 className="card-title">Card title</h4>
+					<div className="card-body ">
+						<h4 className="card-title">{"Card title"}</h4>
 						<p className="card-text">
-							Some quick example text to build on the card title
-							and make up the bulk of the card's content.
+							{
+								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+							}
 						</p>
-						<a href="#" className="btn btn-primary">
-							Go somewhere
+						<a href="#" className="btn btn-primary mx-auto">
+							{"Find Out More!"}
 						</a>
 					</div>
 				</div>
 			</div>
 		</div>
+	);
+};
+
+const End = () => {
+	return (
+		<nav className="navbar navbar-dark bg-dark my-3">
+			<div className="container-fluid justify-content-center">
+				<p className="navbar-text text-light my-4">
+					{"Copyright © Your Webside 2022"}
+				</p>
+			</div>
+		</nav>
 	);
 };
