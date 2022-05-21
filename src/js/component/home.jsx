f7f49c952dba89c225dfcaaf7cc1e2ed427ab570
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-21 21:56:55 +0000

    agrego barra de navegacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,21 +6,66 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<React.Fragment>
+			<Nav />
+		</React.Fragment>
 	);
 };
 
 export default Home;
+
+const Nav = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+			<div className="container-fluid">
+				<a
+					className="navbar-brand"
+					href="#"
+					style={{
+						color: "white",
+						marginRight: "72%",
+						marginLeft: "3%",
+					}}>
+					Start Bootstrap
+				</a>
+				<button
+					className="navbar-toggler btn-light"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a
+								className="nav-link active text-light"
+								aria-current="page"
+								href="#">
+								Home
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-light" href="#">
+								About
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-light" href="#">
+								Services
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-light" href="#">
+								Contact
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
