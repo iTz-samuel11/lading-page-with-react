de9c7d75dc7d2af3706c1ad36651ee35b85a36c7
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-21 23:58:44 +0000

    muevo la funcion Cards a otra carpeta

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Cards } from "./Cards.jsx";
 import { End } from "./End.jsx";
 
 //include images into your bundle
@@ -74,110 +75,98 @@ const Nav = () => {
 	);
 };
 
-const Cards = () => {
-	return (
-		<div className="container">
-			<div className="justifi-content-center m-0 row my-3">
-				<div className="card">
-					<div className="card-body">
-						<h1 className="card-title fs-1">{"A Warm Welcome!"}</h1>
-						<p className="card-text fs-5">
-							{
-								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Ipsa, ipsam,eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."
-							}
-						</p>
-						<a href="#" className="btn btn-primary fs-5">
-							{"Call to action!"}
-						</a>
-					</div>
-				</div>
-			</div>
-			<div className="d-sm-flex">
-				<div className="card text-center">
-					<img
-						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h4 className="card-title">{"Card title"}</h4>
-						<p className="card-text">
-							{
-								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
-							}
-						</p>
-						<a href="#" className="btn btn-primary">
-							{"Find Out More!"}
-						</a>
-					</div>
-				</div>
-				<div className="card mx-1 text-center">
-					<img
-						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h4 className="card-title">{"Card title"}</h4>
-						<p className="card-text">
-							{
-								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
-							}
-						</p>
-						<a href="#" className="btn btn-primary">
-							{"Find Out More!"}
-						</a>
-					</div>
-				</div>
-				<div className="card me-1 text-center">
-					<img
-						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body">
-						<h4 className="card-title">{"Card title"}</h4>
-						<p className="card-text">
-							{
-								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
-							}
-						</p>
-						<a href="#" className="btn btn-primary">
-							{"Find Out More!"}
-						</a>
-					</div>
-				</div>
-				<div className="card text-center">
-					<img
-						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
-						className="card-img-top"
-						alt="..."
-					/>
-					<div className="card-body ">
-						<h4 className="card-title">{"Card title"}</h4>
-						<p className="card-text">
-							{
-								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
-							}
-						</p>
-						<a href="#" className="btn btn-primary mx-auto">
-							{"Find Out More!"}
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
-};
-
-// const End = () => {
+// const Cards = () => {
 // 	return (
-// 		<nav className="navbar navbar-dark bg-dark my-3">
-// 			<div className="container-fluid justify-content-center">
-// 				<p className="navbar-text text-light my-4">
-// 					{"Copyright © Your Webside 2022"}
-// 				</p>
+// 		<div className="container">
+// 			<div className="justifi-content-center m-0 row my-3">
+// 				<div className="card">
+// 					<div className="card-body">
+// 						<h1 className="card-title fs-1">{"A Warm Welcome!"}</h1>
+// 						<p className="card-text fs-5">
+// 							{
+// 								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Ipsa, ipsam,eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."
+// 							}
+// 						</p>
+// 						<a href="#" className="btn btn-primary fs-5">
+// 							{"Call to action!"}
+// 						</a>
+// 					</div>
+// 				</div>
 // 			</div>
-// 		</nav>
+// 			<div className="d-sm-flex">
+// 				<div className="card text-center">
+// 					<img
+// 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+// 						className="card-img-top"
+// 						alt="..."
+// 					/>
+// 					<div className="card-body">
+// 						<h4 className="card-title">{"Card title"}</h4>
+// 						<p className="card-text">
+// 							{
+// 								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+// 							}
+// 						</p>
+// 						<a href="#" className="btn btn-primary">
+// 							{"Find Out More!"}
+// 						</a>
+// 					</div>
+// 				</div>
+// 				<div className="card mx-1 text-center">
+// 					<img
+// 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+// 						className="card-img-top"
+// 						alt="..."
+// 					/>
+// 					<div className="card-body">
+// 						<h4 className="card-title">{"Card title"}</h4>
+// 						<p className="card-text">
+// 							{
+// 								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+// 							}
+// 						</p>
+// 						<a href="#" className="btn btn-primary">
+// 							{"Find Out More!"}
+// 						</a>
+// 					</div>
+// 				</div>
+// 				<div className="card me-1 text-center">
+// 					<img
+// 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+// 						className="card-img-top"
+// 						alt="..."
+// 					/>
+// 					<div className="card-body">
+// 						<h4 className="card-title">{"Card title"}</h4>
+// 						<p className="card-text">
+// 							{
+// 								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+// 							}
+// 						</p>
+// 						<a href="#" className="btn btn-primary">
+// 							{"Find Out More!"}
+// 						</a>
+// 					</div>
+// 				</div>
+// 				<div className="card text-center">
+// 					<img
+// 						src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325-300x195.png"
+// 						className="card-img-top"
+// 						alt="..."
+// 					/>
+// 					<div className="card-body ">
+// 						<h4 className="card-title">{"Card title"}</h4>
+// 						<p className="card-text">
+// 							{
+// 								"Lorem ipsum dolor sit amet, consecteur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+// 							}
+// 						</p>
+// 						<a href="#" className="btn btn-primary mx-auto">
+// 							{"Find Out More!"}
+// 						</a>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
 // 	);
 // };
