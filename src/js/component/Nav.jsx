092f5b4698e1eae2c02a87089fafc80ca10feb91
iTz-samuel11 <samuelandresfaraco@gmail.com>
2022-05-22 00:02:44 +0000

    muevo la funcion Nav a otra carpeta

---
+++ src/js/component/Nav.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+
+export const Nav = () => {
+	return (
+		<nav className="navbar navbar-expand-lg navbar-light bg-dark ">
+			<div className="container-fluid">
+				<a
+					className="navbar-brand text-light"
+					href="#"
+					style={{
+						color: "white",
+						marginRight: "68%",
+						marginLeft: "5%",
+					}}>
+					{"Start Bootstrap"}
+				</a>
+				<button
+					className="navbar-toggler btn-light"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNav"
+					aria-controls="navbarNav"
+					aria-expanded="false"
+					aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div
+					className="collapse navbar-collapse float-right"
+					id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<a
+								className="nav-link active text-light"
+								aria-current="page"
+								href="#">
+								{"Home"}
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-secondary" href="#">
+								{"About"}
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-secondary" href="#">
+								{"Services"}
+							</a>
+						</li>
+						<li className="nav-item">
+							<a className="nav-link text-secondary" href="#">
+								{"Contact"}
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+};
